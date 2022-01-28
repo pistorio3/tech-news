15b0734b63a0bbf84415b1e31822b5172f84ade1
João Victor Pistorio <joao.pistorio@gmail.com>
2022-01-28 11:58:58 -0300

    requirement 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,7 +18,8 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return selector.css("div.tec--card__info > h3 > a::attr(href)").getall()
 
 
 # Requisito 3
